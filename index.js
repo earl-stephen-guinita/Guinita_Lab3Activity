f3aa2b1759fc75438d613e1bb37543e1302522ef
EstebanBraun <guinitaearlstephen@gmail.com>
2026-03-05 04:24:20 +0800

    updated index.js adding a new route to display a list of items

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000;
+const items = ['Apple', 'Banana', 'Orange'];
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -8,18 +9,22 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
 app.get('/about', (req, res) => {
     res.send('About Us');
+});
+app.get('/items', (req, res) => {
+    res.json(items);
 });
 
 app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
